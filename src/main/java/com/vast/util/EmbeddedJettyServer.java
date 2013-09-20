9c94003da4571a27bd038c82b679aaa063bb9e50
David Pratt <dpratt@vast.com>
2013-09-20 12:11:51 -0500

    Added ability to specify context root.

--- src/main/java/com/vast/util/EmbeddedJettyServer.java
+++ src/main/java/com/vast/util/EmbeddedJettyServer.java
@@ -23,6 +23,8 @@ public class EmbeddedJettyServer {
             System.exit(0);
         }
 
+        Args parsedArgs = parseArgs(args);
+
         //probably want to find a better place to put this.
         // Optionally remove existing handlers attached to j.u.l root logger
         SLF4JBridgeHandler.removeHandlersForRootLogger();  // (since SLF4J 1.6.5)
@@ -33,10 +35,9 @@ public class EmbeddedJettyServer {
 
         Server server = new Server();
 
-        if(args.length > 0) {
+        if(parsedArgs.jettyConfig != null) {
             //if there's an explicit jetty.xml, use it
-            String jettyXmlFile = args[0];
-            XmlConfiguration configuration = new XmlConfiguration(new File(jettyXmlFile).toURL());
+            XmlConfiguration configuration = new XmlConfiguration(new File(parsedArgs.jettyConfig).toURL());
             configuration.configure(server);
         } else {
             //otherwise just go with reasonable defaults
@@ -45,9 +46,14 @@ public class EmbeddedJettyServer {
             server.setConnectors(new Connector[]{connector});
         }
 
+        String contextRoot = parsedArgs.contextRoot;
+        if(!contextRoot.startsWith("/")) {
+            contextRoot = "/" + contextRoot;
+        }
+
         WebAppContext context = new WebAppContext();
         context.setServer(server);
-        context.setContextPath("/");
+        context.setContextPath(contextRoot);
 
         //prevent webapps from loading any logging classes
         context.addSystemClass("org.apache.log4j.");
@@ -71,8 +77,38 @@ public class EmbeddedJettyServer {
     private static void usage() {
         System.out.println("==================================================");
         System.out.println("   Usage ---  ");
-        System.out.println(" java -jar your_war_file <optional_jetty.xml>");
+        System.out.println(" java -jar your_war_file ");
+        System.out.println("    --jetty-config=   Path to an optional Jetty XML config file.");
+        System.out.println("    --context-root=   The prefix to use as the servlet context root for this executable war.");
         System.out.println("==================================================");
     }
+
+    //not making this a JavaBean - getters and setters are stupid for a private static class
+    //If you're looking at this and thinking WTF, take a step back and think about *why* you
+    //make everything a bean pattern when it doesn't get you anything. Then, go learn Scala.
+    private static class Args {
+        public String contextRoot = "/";
+        public String jettyConfig = null;
+    }
+
+    private static final String JETTY_CONFIG = "--jetty-config=";
+    private static final String CONTEXT_ROOT = "--context-root=";
+
+    private static Args parseArgs(String[] args) {
+
+        Args parsed = new Args();
+
+        for(int i=0;i<args.length;i++) {
+            if(args[i].startsWith(JETTY_CONFIG)) {
+                parsed.jettyConfig = args[i].substring(JETTY_CONFIG.length());
+            } else if(args[i].startsWith(CONTEXT_ROOT)) {
+                parsed.contextRoot = args[i].substring(CONTEXT_ROOT.length());
+            }
+        }
+
+        return parsed;
+    }
+
+
 
 }
